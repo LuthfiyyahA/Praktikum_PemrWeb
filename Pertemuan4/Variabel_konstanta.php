7feb772c3863198368359cd0b78ad24c80536b6d
LuthfiyyahA <luthfiyyahadzka@gmail.com>
2024-09-20 08:02:21 +0700

    update soal

--- Pertemuan4/Variabel_konstanta.php
+++ Pertemuan4/Variabel_konstanta.php
@@ -4,4 +4,8 @@
     $hasil = $angka1 + $angka2;
     echo "Hasil penjumlahan $angka1 dan $angka2 adalah $hasil.";
 
+    $benar = true;
+    $salah = false;
+    echo "Variabel benar: $benar, Variabel salah: $salah"
+
 ?>
