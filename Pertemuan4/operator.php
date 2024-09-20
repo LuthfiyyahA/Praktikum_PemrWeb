57c11fecac7cd578a56862374c9ff52d5a120155
LuthfiyyahA <luthfiyyahadzka@gmail.com>
2024-09-20 09:10:44 +0700

    perubahan

--- Pertemuan4/operator.php
+++ Pertemuan4/operator.php
@@ -8,3 +8,12 @@
     $hasilBagi = $a / $b;
     $sisaBagi = $a % $b;
     $pangkat = $a ** $b;
+
+    echo "<b>Hasil Operator</b><br>";
+    echo "> Penjumlahan : $a + $b = $hasilTambah<br>";
+    echo "> Pengurangan : $a - $b = $hasilKurang<br>";
+    echo "> Perkalian   : $a * $b = $hasilKali<br>";
+    echo "> Pembagian   : $a / $b = $hasilBagi<br>";
+    echo "> Sisa Bagi   : $a % $b = $sisaBagi<br>";
+    echo "> Perpangkatan: $a ^ $b = $pangkat<br>";
+?>
