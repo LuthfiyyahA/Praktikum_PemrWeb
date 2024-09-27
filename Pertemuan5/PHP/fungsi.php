c7afa7ae1bfcf41497a70d2bb33572d4982d7ecf
LuthfiyyahA <luthfiyyahadzka@gmail.com>
2024-09-27 09:36:23 +0700

    return

--- Pertemuan5/PHP/fungsi.php
+++ Pertemuan5/PHP/fungsi.php
@@ -14,4 +14,11 @@ $saya = "Elok";
 $ucapanSalam = "Selamat pagi";
 
 perkenalan($saya);
+
+function hitungUmur($thn_lahir, $thn_sekarang){
+    $umur = $thn_sekarang - $thn_lahir;
+    return $umur;
+}
+
+echo "Umur saya adalah ". hitungUmur(2004, 2024) ." tahun";
 ?>
