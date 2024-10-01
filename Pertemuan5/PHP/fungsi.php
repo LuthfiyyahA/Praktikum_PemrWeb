ab5ff2b6f040aa4d097155bb6109fd717297dccb
LuthfiyyahA <luthfiyyahadzka@gmail.com>
2024-10-01 10:20:28 +0700

    Fungsi dalam fungsi

--- Pertemuan5/PHP/fungsi.php
+++ Pertemuan5/PHP/fungsi.php
@@ -3,6 +3,8 @@
 function perkenalan($nama, $salam = "Assalamualaikum"){
     echo $salam.", ";
     echo "Perkenalkan, nama saya ".$nama."<br/>";
+
+    echo "Saya berusia ".hitungUmur(1988, 2023)." tahun<br/>";
     echo "Senang berkenalan dengan Anda<br/>";
 }
 
