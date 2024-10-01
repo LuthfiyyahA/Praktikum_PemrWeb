cacba4229250aca315a9ec5756300c6b1f065e2b
LuthfiyyahA <luthfiyyahadzka@gmail.com>
2024-10-01 10:30:06 +0700

    fungsi rekrusif

--- Pertemuan5/PHP/rekursif.php
+++ Pertemuan5/PHP/rekursif.php
@@ -6,4 +6,20 @@ function tampilkanHaloDunia(){
 }
 
 tampilkanHaloDunia();
+
+//Menampilkan angka 1-25
+for ($i=1; $i <= 25 ; $i++) { 
+    echo "Perulangan ke-{$i} <br>";
+}
+
+//Menggunakan fungsi rekrusif
+function tampilkanAngka(int $jumlah, int $indeks = 1){
+    echo "Perulangan ke-{$indeks} <br>";
+
+    if($indeks < $jumlah){
+        tampilkanAngka($jumlah, $indeks + 1);
+    }
+}
+
+tampilkanAngka(20);
 ?>
