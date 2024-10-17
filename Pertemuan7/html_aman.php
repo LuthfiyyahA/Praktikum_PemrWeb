69ce1a932d9db96efc926af936eb25bb84a4b3a5
LuthfiyyahA <luthfiyyahadzka@gmail.com>
2024-10-18 04:36:26 +0700

    J7 Praktikum 4

--- Pertemuan7/html_aman.php
+++ Pertemuan7/html_aman.php
@@ -3,4 +3,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
     $input = $_POST['input'];
 $input = htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
 }
+
+if (isset($_POST['email'])) {
+    $email = $_POST['email'];  
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        
+    } else {
+        
+    }
+}
 ?>
