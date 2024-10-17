f50a313630b75225fa6556fb5dcc3e9e147e23d3
LuthfiyyahA <luthfiyyahadzka@gmail.com>
2024-10-18 06:43:37 +0700

    J7 Praktikum 4

--- Pertemuan7/html_aman.php
+++ Pertemuan7/html_aman.php
@@ -7,9 +7,10 @@ $input = htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
 if (isset($_POST['email'])) {
     $email = $_POST['email'];  
     if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        
+        echo "Input: " . $input . "<br>";
+        echo "Email: " . $email;
     } else {
-        
+        echo "Email tidak valid!";
     }
 }
 ?>
