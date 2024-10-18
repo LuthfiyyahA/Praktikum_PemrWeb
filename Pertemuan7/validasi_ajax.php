0e05c0bbd208f2f69330582e558676154d19e2d8
LuthfiyyahA <luthfiyyahadzka@gmail.com>
2024-10-18 07:11:18 +0700

    J7 Praktikum 7

---
+++ Pertemuan7/validasi_ajax.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Form Input dengan Validasi</title>
+        <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
+    </head>
+    
+    <body>
+        <h1>Form Input dengan Validasi</h1>
+        <form id="myForm" method="post">
+            <label for="nama">Nama:</label>
+            <input type="text" id="nama" name="nama">
+            <span id="nama-error" style="color:red;"></span>
+            <br>
+            <label for="email">Email:</label>
+            <input type="text" id="email" name="email">
+            <span id="email-error" style="color:red;"></span>
+            <br>
+            <input type="submit" value="Submit">
+        </form>
+        <div id="result"></div>
+        <script>
+            $(document).ready(function () {
+                $("#myForm").submit(function (event) {
+                    event.preventDefault(); // Mencegah pengiriman form secara default
+                    var nama = $("#nama").val();
+                    var email = $("#email").val();
+                    var valid = true;
+                    if (nama === "") {
+                        $("#nama-error").text("Nama harus diisi.");
+                        valid = false;
+                    } else {
+                        $("#nama-error").text("");
+                    }
+                    if (email === "") {
+                        $("#email-error").text("Email harus diisi.");
+                        valid = false;
+                    } else {
+                        $("#email-error").text("");
+                    }
+                    if (valid) {
+                        $.ajax({
+                            url: "proses_validasi.php",
+                            type: "POST",
+                            data: { nama: nama, email: email },
+                            success: function (response) {
+                                $("#result").html(response);
+                            }
+                        });
+                    }
+                });
+            });
+        </script>
+    </body>
+</html>
